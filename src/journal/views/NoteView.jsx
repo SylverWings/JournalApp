069eb79d4df6fcbb78482cd8b11a7ae96c976f51
lionel.garcia <lionel.garcia@t-innova.com>
2023-01-02 16:15:38 +0100

    set button to push image to Cloudinary

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,9 @@
-import { SaveOutlined } from '@mui/icons-material'
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { SaveOutlined, UploadOutlined } from '@mui/icons-material'
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material'
 import { ImageGallery } from '../components/ImageGallery'
 import { useForm } from '../../hooks'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { setActiveNote, startSaveNotes } from '../../store/journal'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css'
@@ -23,6 +23,8 @@ export const NoteView = () => {
         return newDate.toUTCString();
     }, [date])
 
+    const fileInputRef = useRef()
+
     useEffect(()=>{
         dispatch( setActiveNote(formState) )
     }, [formState])
@@ -37,6 +39,11 @@ export const NoteView = () => {
         dispatch( startSaveNotes() )
     }
 
+    const onFileInputChange = ({target}) =>{
+
+        if( target.files === 0)return
+    }
+
     return (
         <Grid
             className="animate__animated animate__fadeIn animate__faster" 
@@ -47,6 +54,22 @@ export const NoteView = () => {
                 <Typography fontSize={ 39 } fontWeight='light'>{ dateString }</Typography>
             </Grid>
             <Grid item>
+                
+                <input 
+                    type="file" 
+                    multiple
+                    ref={ fileInputRef }
+                    onChange={ onFileInputChange }
+                    style={{ display: 'none' }}
+                />
+
+                <IconButton
+                    color='primary'
+                    disabled={ isSaving }
+                    onClick={ ()=> fileInputRef.current.click() }
+                >
+                    <UploadOutlined />
+                </IconButton>
                 <Button 
                     color='primary' 
                     sx={{ padding: 2}} 
